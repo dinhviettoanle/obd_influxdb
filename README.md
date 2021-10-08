56e64741906bc23bbb2a47d7c421b768d00d699a
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2021-10-08 21:58:01 +0200

    basics: add indications and synchronous import

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# obd_influxdb
+# OBD : InfluxDB
+
+## Manips de base
+### Importer des données
+- Installer les `requirements.txt` 
+- Lancer une instance InfluxDB (`influxd` ou `<path_to_influxdb>\influxd.exe`)
+- Créer un bucket `mybucket` : sur `localhost:8086` > se connecter > Data > Buckets > Create Bucket
+- Modifier le fichier `basics_influx.py` : configurer les variables dans la section "Init client"
+- Lancer `python ./basics_influx.py --command write_csv_sync`
+- (Essayer de visualiser les données, s'il n'y a pas de résultats, essayer : `python ./basics_influx.py --command write_csv_async`)
+
+### Visualiser toutes les données
+- Aller dans Explore > Query Builder
+- Entrer :
+<code>from(bucket: "mybucket")
+&nbsp;&nbsp;&nbsp;&nbsp;|> range(start: 0, stop: now())</code>
+- Submit
+- (Toggle le "View Raw Data" si besoin)
--- basics_influx.py
+++ basics_influx.py
@@ -2,33 +2,41 @@
 # Et ca : https://influxdb-client.readthedocs.io/en/stable/
 # Peut etre utile : https://github.com/fabio-miranda/csv-to-influxdb
 
+import argparse
+
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client import InfluxDBClient, Point, WriteOptions
 
 import rx
 from rx import operators as ops
-
-import argparse
 from collections import OrderedDict
 from csv import DictReader
+import pandas as pd
+from tqdm.autonotebook import tqdm
+from pprint import pprint
 
 
+# ===== Init client =====
 
-# Init client
-bucket = "mybucket"
-org = "obd_influxdb"
-token = None
+# Name of the bucket
+bucket = "mybucket" 
+# Name of the organization -> in the "profile picture" tab on the left, under the username
+org = "" 
+# Token of the user -> "data" tab on the left > tab "Tokens" > click on "{username}'s Token"
+token = "" 
 url="http://localhost:8086"
 
+# =======================
 
+datafile = 'data/vix-daily.csv'
 
 
 
 def write_db():
+    # Writes a single line in the database
+    
     write_api = client.write_api(write_options=SYNCHRONOUS)
-
-    # TODO : Gérer le timestamp
     p = influxdb_client.Point("my_measurement").tag("location", "Prague").field("temperature", 25.3) 
     write_api.write(bucket=bucket, org=org, record=p)
     print("Done !")
@@ -37,28 +45,31 @@ def write_db():
 
 
 def query_db():
+    # Queries the database 
+    # required : financial-anaysis is already created
+
     query_api = client.query_api()
     query = f"""from(bucket:"{bucket}")\
-    |> range(start: -10m)\
-    |> filter(fn:(r) => r._measurement == "my_measurement")\
-    |> filter(fn: (r) => r.location == "Prague")\
-    |> filter(fn:(r) => r._field == "temperature" )"""
+    |> range(start: -2y)\
+    |> filter(fn:(r) => r._measurement == "financial-analysis")\
+    |> filter(fn:(r) => r._field == "open" )"""
 
     result = query_api.query(org=org, query=query)
     results = []
     for table in result:
         for record in table.records:
-            results.append((record.get_field(), record.get_value()))
+            results.append((record.get_time().strftime("%d/%m/%Y, %H:%M:%S"), record.get_field(), record.get_value()))
 
-    print(results)
+    pprint(results)
 
 
 
 def delete_db():
+    # Removes the measurement "financial-analysis"
+
     delete_api = client.delete_api()
     start = "1970-01-01T00:00:00Z"
     stop = "2022-02-01T00:00:00Z"
-    delete_api.delete(start, stop, '_measurement="my_measurement"', bucket=bucket, org=org)
     delete_api.delete(start, stop, '_measurement="financial-analysis"', bucket=bucket, org=org)
     print("Done !")
 
@@ -83,18 +94,13 @@ def parse_row(row: OrderedDict):
     :return: Parsed csv row to [Point]
     """
 
-    """
-        For better performance is sometimes useful directly create a LineProtocol to avoid unnecessary escaping overhead:
-        """
-        # from pytz import UTC
-        # import ciso8601
-        # from influxdb_client.client.write.point import EPOCH
-        #
-        # time = (UTC.localize(ciso8601.parse_datetime(row["Date"])) - EPOCH).total_seconds() * 1e9
-        # return f"financial-analysis,type=vix-daily" \
-        #        f" close={float(row['VIX Close'])},high={float(row['VIX High'])},low={float(row['VIX Low'])},open={float(row['VIX Open'])} " \
-        #        f" {int(time)}"
-
+    # Du coup, va renvoyer 4 points différents dans le _measurement "financial-analysis":
+    # - {"type" : "vix-daily", "_field" : "open", "_value" : VIX_Open}
+    # - {"type" : "vix-daily", "_field" : "high", "_value" : VIX_High}
+    # - {"type" : "vix-daily", "_field" : "low", "_value" : VIX_Low}
+    # - {"type" : "vix-daily", "_field" : "close", "_value" : VIX_Close}
+    # D'où taille du dataset InfluxDB = 4*taille_du_csv, vu qu'on aura 4 tables
+    
     return Point("financial-analysis") \
         .tag("type", "vix-daily") \
         .field("open", float(row['VIX Open'])) \
@@ -106,11 +112,12 @@ def parse_row(row: OrderedDict):
 
 
 
-def write_csv():
+def write_csv_async():
     # From https://influxdb-client.readthedocs.io/en/stable/usage.html#how-to-efficiently-import-large-dataset
+    # Plus efficace, mais pas tout compris
 
     data = rx \
-        .from_iterable(DictReader(open('data/vix-daily.csv', 'r'))) \
+        .from_iterable(DictReader(open(datafile, 'r'))) \
         .pipe(ops.map(lambda row: parse_row(row)))
 
     write_api = client.write_api(write_options=WriteOptions(batch_size=50_000, flush_interval=10_000))
@@ -118,9 +125,27 @@ def write_csv():
     write_api.close()
     print("Done !")
 
-    # Pour afficher dans le GUI : 
-    #    from(bucket:"mybucket")
-    #       |> range(start: 0, stop: now())
+
+
+
+def write_csv_sync():
+    # Writes in the database lines per lines
+
+    # API init
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    # Data import as df
+    df = pd.read_csv(datafile, sep=',')
+
+    # Import loop on rows of df
+    n_rows = len(df)
+    pbar = tqdm(total=n_rows)
+    for i, row in df.iterrows():
+        p = parse_row(row)
+        write_api.write(bucket=bucket, org=org, record=p)
+        pbar.update(1)
+    pbar.close()
+    print("Done !")
 
 
 
@@ -136,15 +161,21 @@ if __name__ == '__main__':
         'write' : write_db,
         'query' : query_db,
         'delete' : delete_db,
-        'write_csv' : write_csv,
+        'write_csv_async' : write_csv_async,
+        'write_csv_sync' : write_csv_sync,
         }
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--token', required=True)
     parser.add_argument('--command', choices=FUNCTION_MAP.keys(), required=True)
+    parser.add_argument('--token', required=False)
+    parser.add_argument('--org', required=False)
     args = parser.parse_args()
 
-    token = args.token
+    if args.token is not None and token == "":
+        token = args.token
+    if args.org is not None and org == "":
+        org = args.org
+
     client = influxdb_client.InfluxDBClient(
         url=url,
         token=token,
